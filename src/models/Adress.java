a7c6a3ac3bf8edd4933b9a7b6e34086045b24c66
check1tos <nikita.cernenko07@gmail.com>
2026-03-18 11:59:24 +0200

    extended adress class

--- src/models/Adress.java
+++ src/models/Adress.java
@@ -6,7 +6,51 @@ public class Adress {
 	
 	private City city;
 	private String streetOrHouseTitle;
-	private String houseNo;
+	private int	 houseNo;
+	
+	public Adress() {
+		city = City.Ventspils;
+		streetOrHouseTitle = "Null";
+		houseNo = 0;
+	}
+	
+	public Adress(City city, String streetorhousetitle, int houseno) {
+		
+		this.city = city;
+		streetOrHouseTitle = streetorhousetitle;
+		houseNo = houseno;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+	}
+
+	public String getStreetOrHouseTitle() {
+		return streetOrHouseTitle;
+	}
+
+	public void setStreetOrHouseTitle(String streetOrHouseTitle) {
+		this.streetOrHouseTitle = streetOrHouseTitle;
+	}
+
+	public int getHouseNo() {
+		return houseNo;
+	}
+
+	public void setHouseNo(int houseNo) {
+		this.houseNo = houseNo;
+	}
+
+	@Override
+	public String toString() {
+		return "Adress [city=" + city + ", streetOrHouseTitle=" + streetOrHouseTitle + ", houseNo=" + houseNo + "]";
+	}
+	
+	
 	
 	
 }
